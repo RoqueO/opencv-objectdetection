89f5c65b0de2f56d400da3cf13f5217145930edc
Roque Obusan <robusan@gmail.com>
2024-05-04 18:13:50 -0700

    feat: added windows to display each step
    3 windows are created to show each step of the process from background
    subtraction to erosion and then finally displaying the found objects

--- ObjectDetect.py
+++ ObjectDetect.py
@@ -2,53 +2,63 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-deviceID = 0;           # 0 = open default camera
-apiID = cv2.CAP_ANY;    # 0 = autodetect default API
+def createwindows(window_names: list, width=1366, height=768):
+    for i, window_name in enumerate(window_names):``
+        cv2.namedWindow(window_name)
+        x = i % 2 * width
+        y = i // 2 * height
+        cv2.moveWindow(window_name, x, y)
 
-cap = cv2.VideoCapture(deviceID, apiID)
-backSub = cv2.createBackgroundSubtractorMOG2()
-if not cap.isOpened():
-    print("Error opening video file")
+if __name__ == "__main__":
+    deviceID = 0;           # 0 = open default camera
+    apiID = cv2.CAP_ANY;    # 0 = autodetect default API
 
-while cap.isOpened():
-    # Capture frame-by-frame
-        ret, frame = cap.read()
-        if ret:
-            # Apply background subtraction
-            fg_mask = backSub.apply(frame, fgmask=None, learningRate=0.2)
+    cap = cv2.VideoCapture(deviceID, apiID)
+    backSub = cv2.createBackgroundSubtractorMOG2()
+    if not cap.isOpened():
+        print("Error opening video file")
 
-            # apply global threshold to remove shadows
-            retval, mask_thresh = cv2.threshold( fg_mask, 150, 255, cv2.THRESH_BINARY)
+    createwindows(window_names=['Subtractor', 'Eroded', 'Processed'])
 
-            # set the kernal
-            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
-            # Apply erosion
-            mask_eroded = cv2.morphologyEx(mask_thresh, cv2.MORPH_OPEN, kernel)
+    while cap.isOpened():
+        # Capture frame-by-frame
+            ret, frame = cap.read()
+            if ret:
+                # Apply background subtraction
+                fg_mask = backSub.apply(frame, fgmask=None, learningRate=0.2)
 
-            # Find contours
-            contours, hierarchy = cv2.findContours(mask_eroded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            # print(contours)
-            #frame_ct = cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+                # apply global threshold to remove shadows
+                retval, mask_thresh = cv2.threshold( fg_mask, 150, 255, cv2.THRESH_BINARY)
 
-            min_contour_area = 1000  # Define your minimum area threshold
-            large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area]
+                # set the kernal
+                kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
+                # Apply erosion
+                mask_eroded = cv2.morphologyEx(mask_thresh, cv2.MORPH_OPEN, kernel)
 
-            # Draw bounding boxes
-            frame_out = frame.copy()
-            for cnt in large_contours:
-                x, y, w, h = cv2.boundingRect(cnt)
-                frame_out = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 200), 3)
- 
-            # Display the resulting frame
-            cv2.imshow('Preview', frame_out)
-            cv2.imshow('Background', fg_mask)
-            cv2.imshow('Eroded', mask_eroded)
-            if cv2.waitKey(25) & 0xFF == ord('q'): 
-                break
+                # Find contours
+                contours, hierarchy = cv2.findContours(mask_eroded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                # print(contours)
+                #frame_ct = cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
 
-# When everything done, release 
-# the video capture object 
-cap.release() 
-  
-# Closes all the frames 
-cv2.destroyAllWindows() 
+                min_contour_area = 1000  # Define your minimum area threshold
+                large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area]
+
+                # Draw bounding boxes
+                frame_out = frame.copy()
+                for cnt in large_contours:
+                    x, y, w, h = cv2.boundingRect(cnt)
+                    frame_out = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 200), 3)
+    
+                # Display the resulting frame
+                cv2.imshow('Processed', frame_out)
+                cv2.imshow('Subtractor', fg_mask)
+                cv2.imshow('Eroded', mask_eroded)
+                if cv2.waitKey(25) & 0xFF == ord('q'): 
+                    break
+
+    # When everything done, release 
+    # the video capture object 
+    cap.release() 
+    
+    # Closes all the frames 
+    cv2.destroyAllWindows() 
